e7abe9db7e52c1f60be4a148c8d926bddd9cb3e9
Aryav_Lahiri <aryav.lahiri@kcl.ac.uk>
2023-09-25 12:42:33 +0100

    Create dictionary with each item paired with the n.o. of times it appears in list

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    items = (str(x) for x in items)
     frequencies = {}
-    # Your code goes here
+    
+    for item in items:
+        if item not in frequencies:
+            frequencies[item] = 1
+        else:
+            frequencies[item] += 1
+    
     return frequencies
+
+
+
+
+  
